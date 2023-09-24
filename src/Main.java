0e9c2ffd4de13ca29eadd7f3365451a510e4934d
ElenaChalova <03-03-11@mail.ru>
2023-09-24 16:36:26 +0700

    Реализовали игру

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        final String [] choices ={"Камень", "ножницы", "бумага"};
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int compChoice = random.nextInt(choices.length); //Генерирует случайный индекс
+        int exit;
+
+        System.out.println("Начало игры");
+        System.out.println("Выберите вариант");
+        System.out.println("0.Камень\n"+"1.Ножницы\n"+"2.Бумага\n");
+        int playerChoice=scanner.nextInt();
+        System.out.println(compChoice);
+        if (playerChoice==compChoice){
+            System.out.println("Ничья");
+        }
+        else if (playerChoice==0 && compChoice==1 || playerChoice==1 && compChoice==2 || playerChoice==2 && compChoice==0){
+            System.out.println("Победил игрок");
+        }
+        else System.out.println("Победил компьютер");
     }
 }
